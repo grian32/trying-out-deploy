d4d917d24f13caaa319f445949d87c6881ebf822
Grian <grianguy32@gmail.com>
2021-06-29 05:07:20 +0300

    gitignore, crux router

---
+++ crux-router.ts
@@ -0,0 +1,10 @@
+import { router } from "https://crux.land/router@0.0.3";
+
+addEventListener("fetch", (event: FetchEvent) => {
+  event.respondWith(
+    router({
+      "GET@/": (_req: Request) => new Response("Hello get!", { status: 200 }),
+      "POST@/": (_req: Request) => new Response("Hello post!", { status: 200 }),
+    })(event.request),
+  );
+});
