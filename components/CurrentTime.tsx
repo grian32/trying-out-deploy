52bdf27767b2c0f55971d3a734e74c4cc6362398
Grian <grianguy32@gmail.com>
2021-07-13 12:42:09 +0300

    state

--- components/CurrentTime.tsx
+++ components/CurrentTime.tsx
@@ -1,13 +1,14 @@
 import React, { h } from "https://x.lcas.dev/preact@10.5.12/mod.js";
 import { format } from "https://deno.land/std@0.101.0/datetime/mod.ts";
+import { useState } from "https://x.lcas.dev/preact@10.5.4/hooks.js";
 
 export default function CurrentTime() {
-    let date = new Date();
+    let [date, setDate] = useState(new Date());
 
     setInterval(() => {
         let newDate = new Date();
         if (date <= newDate) {
-            date = newDate;
+            setDate(newDate);
         }
     }, 1000)
 
