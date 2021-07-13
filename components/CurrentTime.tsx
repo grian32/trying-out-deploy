f0d33440f5d54de9137ffbf9e2004c35c4e20af8
Grian <grianguy32@gmail.com>
2021-07-13 12:50:37 +0300

    am pm

--- components/CurrentTime.tsx
+++ components/CurrentTime.tsx
@@ -6,6 +6,6 @@ export default function CurrentTime() {
     let date = new Date()
 
     return (
-        <p id="parsedDate"> { format(date, "h:mm:ss") } UTC </p>
+        <p id="parsedDate"> { format(date, "h:mm:ss a") } UTC </p>
     )
 }
