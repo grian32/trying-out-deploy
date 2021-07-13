5215c00ac787875584dafb456bb4638555eae1a8
Grian <grianguy32@gmail.com>
2021-07-13 12:47:40 +0300

    set the interval in useeffect

--- components/CurrentTime.tsx
+++ components/CurrentTime.tsx
@@ -1,16 +1,17 @@
 import React, { h } from "https://x.lcas.dev/preact@10.5.12/mod.js";
 import { format } from "https://deno.land/std@0.101.0/datetime/mod.ts";
-import { useState } from "https://x.lcas.dev/preact@10.5.12/hooks.js";
+import { useState, useEffect } from "https://x.lcas.dev/preact@10.5.12/hooks.js";
 
 export default function CurrentTime() {
     let [date, setDate] = useState(new Date());
-
-    setInterval(() => {
-        let newDate = new Date();
-        if (date <= newDate) {
-            setDate(newDate);
-        }
-    }, 1000)
+    useEffect(() => {
+        setInterval(() => {
+            let newDate = new Date();
+            if (date <= newDate) {
+                setDate(newDate);
+            }
+        }, 1000)
+    })
 
     return (
         <p id="parsedDate"> { format(date, "h:mm:ss")}</p>
