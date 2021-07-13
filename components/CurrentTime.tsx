2c4669e0bdf2ef51eaaab92318c89f09e3c51132
Grian <grianguy32@gmail.com>
2021-07-13 12:46:13 +0300

    downgrade preact

--- components/CurrentTime.tsx
+++ components/CurrentTime.tsx
@@ -1,6 +1,6 @@
-import React, { h } from "https://x.lcas.dev/preact@10.5.4/mod.js";
+import React, { h } from "https://x.lcas.dev/preact@10.5.12/mod.js";
 import { format } from "https://deno.land/std@0.101.0/datetime/mod.ts";
-import { useState } from "https://x.lcas.dev/preact@10.5.4/hooks.js";
+import { useState } from "https://x.lcas.dev/preact@10.5.12/hooks.js";
 
 export default function CurrentTime() {
     let [date, setDate] = useState(new Date());
